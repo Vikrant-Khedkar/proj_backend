6556d308c2f955c956d98e0aa23457f9e3976ab8
vikrant-khedkar <vikrantkhedkar2720@gmail.com>
2023-08-09 16:25:04 +0530

    upload working

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
-const ipfsClient = require('ipfs-http-client');
 const express = require('express');
 const bodyParser = require('body-parser');
-const fileUpload =  require('express-fileupload');
-const fs  =  require('fs');
-const ipfs =  new ipfsClient({host:'localhost',port:'5001',protocol:'http'});
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const ipfsAPI = require('ipfs-api');
+
+const ipfs = ipfsAPI("/ip4/127.0.0.1/tcp/5001");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.send('HI');
 });
 
-app.post('/upload',(req,res)=>{
-    const file = req.files.file;
-    const fileName = req.body.fileName;
-    const filePath = 'files/'+ fileName;
+app.post('/upload', (req, res) => {
+  const file = req.files.file;
+  const fileName = req.body.fileName;
+  const filePath = 'files/' + fileName;
 
-    file.mv(filePath,async (err) => {
-        if(err) {
-            console.log("Error:failed to downlaod file");
-            return res.status(500).send(err);
-        }
-        const fileHash = await addFile(fileName,filePath);
-        fs.unlink(filePath,(err)=> {
-            if(err) console.log(err);
-        });
-        res.send({fileName,fileHash});
-    })
-})
-const addFile = async(fileName,filePath) =>{
-    const file = fs.readFileSync(filePath);
-    const fileAdded = await ipfs.add({path:fileName,content:file});
-    const fileHash = fileAdded[0].hash;
-    
-    return fileHash;
+  file.mv(filePath, async (err) => {
+    if (err) {
+      console.log('Error: failed to download file');
+      return res.status(500).send(err);
+    }
+    const fileHash = await addFile(fileName, filePath);
+    fs.unlink(filePath, (err) => {
+      if (err) console.log(err);
+    });
+    res.send({ fileName, fileHash });
+  });
+});
+
+const addFile = async (fileName, filePath) => {
+  const fileAdded = await new Promise((resolve, reject) => {
+    const fileStream = fs.createReadStream(filePath);
+    const ipfsFileObj = {
+      path: fileName,
+      content: fileStream,
+    };
+    ipfs.add(ipfsFileObj, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  const fileHash = fileAdded[0].hash;
+  return fileHash;
 };
 
-app.listen(3000,() => {
-    console.log("server is listening on port 3000")
+app.listen(3000, () => {
+  console.log('server is listening on port 3000');
 });
